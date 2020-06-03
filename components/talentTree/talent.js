bc71e2f865c84827861cef093b2a0fdd50973482
niviso <nikki.sollid@gmail.com>
2020-06-03 16:26:12 +0200

    Wip

--- components/talentTree/talent.js
+++ components/talentTree/talent.js
@@ -4,10 +4,10 @@ import { TouchableOpacity, Text } from 'react-native';
 import Styles from './styles.scss';
 
 export default function Talent(props){
-  const {data,onclick} = props;
+  const {data,onclick,id} = props;
 return(
-  <TouchableOpacity style={Styles.Talent} onPress={() => onclick(data.id)}>
-  <Icon fill={false} name={data.icon}/>
+  <TouchableOpacity style={Styles.Talent} onPress={() => onclick(data.id-1)}>
+  <Icon fill={false} name={data.icon} active={data.active}/>
   <Text style={{fontSize: 10,textAlign: 'center'}}>{data.name}{"\n"}{data.currentPoints}/{data.maxPoints}</Text>
   </TouchableOpacity>
 )
--- components/talentTree/tree.js
+++ components/talentTree/tree.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import { TalentContext } from "../../context/talentContext";
 import Talent from './talent';
 import { Text, View,Image,Dimensions } from 'react-native';
@@ -6,16 +6,27 @@ import Styles from './styles.scss';
 
 export default function Tree(props){
   const [state,setState] = useContext(TalentContext);
+  const [totalTalentPoints,setTotalTalentPoints] = useState(1);
   const window = Dimensions.get("window");
 
   const addPoint = (id) => {
     let tmp = JSON.parse(JSON.stringify(state));
-    if(tmp[id-1].currentPoints < tmp[id-1].maxPoints){
-    tmp[id-1].currentPoints++;
+    let talent = tmp[id];
+    if(!talent){
+      return;
     }
 
-    if(tmp[id-1].currentPoints == tmp[id-1].maxPoints){
-      //Unlock
+    if(totalTalentPoints < talent.requiredPoints) {
+      return;
+    }
+    if(talent.currentPoints < talent.maxPoints){
+    talent.currentPoints++;
+    talent.active = true;
+    setTotalTalentPoints(totalTalentPoints+1);
+    }
+
+    if(talent.currentPoints == talent.maxPoints && talent.unlocksId){
+      tmp[talent.unlocksId].active = true
     }
 
 
@@ -23,7 +34,7 @@ export default function Tree(props){
   }
 
   const talents = state.map((talent,index) =>
-  <View key={index} style={{position: 'absolute', left: (talent.slot * (window.width/4)-75),top: talent.level * 100,width:(window.width/4)}}>
+  <View key={index} id={index} style={{position: 'absolute', left: (talent.slot * (window.width/4)-75),top: talent.level * 100,width:(window.width/4)}}>
   <Talent data={talent} onclick={addPoint}/>
   </View>
   );
@@ -31,6 +42,8 @@ return(
 
   <View style={Styles.Tree}>
   {talents}
+  <Text>{totalTalentPoints}</Text>
+
   </View>
 )
 }
